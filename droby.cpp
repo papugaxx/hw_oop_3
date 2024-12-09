d5eaa95672977924dc7ec95ee3fd5a4f386e2fb0
papugaxx <tc10762@gmail.com>
2024-12-09 20:53:58 +0200

    Update droby.cpp

--- droby.cpp
+++ droby.cpp
@@ -1,39 +1,39 @@
-#include "droby.h"
+#include "Fractions.h"
 #include <iostream>
 using namespace std;
 
-void droby::setValues(int num, int denom) {
-    numerator = num;  
-    denominator = denom;  
+void Fractions::setValues(int num, int denom) {
+    numerator = num;
+    denominator = denom;
 }
 
-void droby::print() {
+void Fractions::print() {
     cout << numerator << "/" << denominator;
 }
 
-droby droby::add(droby other) {
-    droby result;
+Fractions Fractions::operator+(const Fractions& other) {
+    Fractions result;
     result.numerator = numerator * other.denominator + other.numerator * denominator;
     result.denominator = denominator * other.denominator;
     return result;
 }
 
-droby droby::subtract(droby other) {
-    droby result;
+Fractions Fractions::operator-(const Fractions& other) {
+    Fractions result;
     result.numerator = numerator * other.denominator - other.numerator * denominator;
     result.denominator = denominator * other.denominator;
     return result;
 }
 
-droby droby::multiply(droby other) {
-    droby result;
+Fractions Fractions::operator*(const Fractions& other) {
+    Fractions result;
     result.numerator = numerator * other.numerator;
     result.denominator = denominator * other.denominator;
     return result;
 }
 
-droby droby::divide(droby other) {
-    droby result;
+Fractions Fractions::operator/(const Fractions& other) {
+    Fractions result;
     result.numerator = numerator * other.denominator;
     result.denominator = denominator * other.numerator;
     return result;
